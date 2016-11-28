43081b766815fcb60a35b9675bbced9242b7ad77
HoangNH <hoangnh@evolableasia.vn>
2016-11-28 08:42:09 +0700

    update

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -19,16 +19,16 @@
           <div class="item col-md-2">
             <div class="item-product5">
               <div class="product-thumb product-thumb5">
-                <a href="{{ $sp->url }}" target="_blank" class="product-thumb-link">
+                <a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank" class="product-thumb-link">
                   <img class="first-thumb" src="{{ $sp->thumbnail_url }}" alt=""/> 
                   <img class="second-thumb" src="{{ $sp->thumbnail_url }}" alt=""/>                
                 </a>
                 <div class="product-info-cart">                
-                  <a href="{{ $sp->url }}" target="_blank" class="addcart-link"><i class="fa fa-shopping-basket"></i>  Mua ngay</a>
+                  <a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank" class="addcart-link"><i class="fa fa-shopping-basket"></i>  Mua ngay</a>
                 </div>
               </div>
               <div class="product-info5">
-                <h3 class="title-product"><a href="{{ $sp->url }}" target="_blank">{{ $sp->name }}</a></h3>
+                <h3 class="title-product"><a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank">{{ $sp->name }}</a></h3>
                 <div class="info-price">
                   <span>{{ Helper::showPriceAff($sp->aff_price) }} đ</span>
                   <del>{{ Helper::showPriceAff($sp->aff_price_old) }} đ</del>
@@ -63,16 +63,16 @@
           <div class="item col-md-2">
             <div class="item-product5">
               <div class="product-thumb product-thumb5">
-                <a href="{{ $sp->url }}" target="_blank" class="product-thumb-link">
+                <a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank" class="product-thumb-link">
                   <img class="first-thumb" src="{{ $sp->thumbnail_url }}" alt=""/> 
                   <img class="second-thumb" src="{{ $sp->thumbnail_url }}" alt=""/>                
                 </a>
                 <div class="product-info-cart">                
-                  <a href="{{ $sp->url }}" target="_blank" class="addcart-link"><i class="fa fa-shopping-basket"></i>  Mua ngay</a>
+                  <a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank" class="addcart-link"><i class="fa fa-shopping-basket"></i>  Mua ngay</a>
                 </div>
               </div>
               <div class="product-info5">
-                <h3 class="title-product"><a href="{{ $sp->url }}" target="_blank">{{ $sp->name }}</a></h3>
+                <h3 class="title-product"><a href="https://pub.accesstrade.vn/deep_link/4463492395743736163?url={{ $sp->url }}" target="_blank">{{ $sp->name }}</a></h3>
                 <div class="info-price">
                   <span>{{ Helper::showPriceAff($sp->aff_price) }} đ</span>
                   <del>{{ Helper::showPriceAff($sp->aff_price_old) }} đ</del>
